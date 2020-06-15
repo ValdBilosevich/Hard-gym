9d74fab7ebd07ae09ec952d50702a61dfad01ee5
ValdBilosevich <66934741+ValdBilosevich@users.noreply.github.com>
2020-06-15 08:11:58 +0300

    commit
    web

---
+++ menu.js
@@ -0,0 +1,12 @@
+ function menu_mob()
+{
+    let el = document.getElementById('menu_m');
+    if(el.style.display!='block')
+    {
+        el.style.display ='block';
+    }
+    else
+    {
+        el.style.display ='none';
+    }
+}
